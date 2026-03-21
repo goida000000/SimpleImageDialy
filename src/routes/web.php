2db64d8a884837e2201f6dad93f53a82507136fc
goida000000 <goida.000000@gmail.com>
2026-03-21 18:58:28 +0900

    v1.3 develop release
    投稿内容確認画面の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,4 +5,8 @@ use App\Http\Controllers\DiaryController;
 
 Route::resource('diaries', DiaryController::class);
 Route::get('/', [DiaryController::class,'index']);
+Route::post('/diaries/confirm', [DiaryController::class, 'confirm'])->name('diaries.confirm');
+// Route::post('/diaries/{id}/confirm', [DiaryController::class, 'confirmUpdate'])->name('diaries.confirmUpdate');
+Route::match(['put', 'post', 'get'], '/diaries/{id}/confirm', [DiaryController::class, 'confirmUpdate'])->name('diaries.confirmUpdate');
+
 Route::redirect('/', '/diaries');
